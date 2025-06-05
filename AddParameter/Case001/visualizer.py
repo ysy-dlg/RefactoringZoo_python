4f1d71868f3868dd55a9889af135e4bdaff43b3b
ysy-dlg <yang.shiyu5329@gmail.com>
2025-06-06 07:53:07 +0900

    Add Add Parameter Case001 (before refactoring)

---
+++ AddParameter/Case001/visualizer.py
@@ -0,0 +1,7 @@
+def visualize_network(model_name):
+    print(f"[INFO] Visualizing model: {model_name}")
+    print("Generating plot at default DPI = 100")
+
+# Caller
+if __name__ == "__main__":
+    visualize_network("resnet50")
